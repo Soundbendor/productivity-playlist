59024132bd24e32dbf10ba729f0ace682e25f3b9
Shaurya Gaur <shauryavrat@live.com>
2020-01-13 21:59:28 -0800

    updated DataFrame access and added more inputs

--- knn.py
+++ knn.py
@@ -8,7 +8,7 @@ from sklearn.neighbors import NearestNeighbors
 # (this line just loads the first 2000 songs ... I figured that'd be enough for a first run)
 songdata = pd.read_csv("deam-data\\annotations\\annotations averaged per song\song_level\static_annotations_averaged_songs_1_2000.csv", header=0, index_col=0, usecols=[0, 1, 3])
 print("read song data")
-print(songdata[1000])
+print(songdata.loc[156, :])
 
 # graphing around here wouldn't be a terrible idea
 
@@ -21,9 +21,14 @@ print("trained data ... fingers crossed")
 origin = int(input("Choose a number for a starting song: "))
 destination = int(input("Choose a number for an ending song: "))
 current = origin
+print(songdata.loc[current, :]) #TODO: check if the number is in the list of indexes or else errors will happen (if you want)
+print(songdata.loc[destination, :])
 
 # input the time needed to get from starting to destination
 # since these are 30 second song clips, divide the time to figure out how many songs we need (n_songs_needed) (round down if necessary)
+time_reqd = int(input("How many minutes between your start and destination? "))
+n_songs_reqd = 
+
 # create a list of numbers (for songs) to store our "path" - let's call it song_list
 # 
 # while (current != destination && n_songs_needed > 0):
@@ -35,4 +40,3 @@ current = origin
 #   calculate the between between each of those and the desired slope and distance
 #   put "current" in "song_list"
 #   select the song which has the ratio closest to 1.00 to be the new value of "current"
-print("Test")
