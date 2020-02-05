9e389f67ed56d38434dd580810df6e851dec345b
Shaurya Gaur <shauryavrat@live.com>
2020-02-05 14:44:14 -0800

    created spotify info grabber

--- spotigrab.py
+++ spotigrab.py
@@ -20,17 +20,84 @@ try:
 except:
     print('Token is not accessible for ' + username)
 
-songtitles = pd.read_csv("deam-data\\metadata\\metadata_2015.csv", header=0, usecols=[2, 3])
+songdata = pd.read_csv("deezer-data\\train.csv", header=0, index_col=0, usecols=[0, 3, 4, 5, 6])
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
-print(songtitles.iloc[0])
+print(songdata.iloc[0])
 
 # merge the artist and track to get a single search item
 joined_titles = []
 
-for i in range(len(songtitles)):
-    joined_titles.append(songtitles.iloc[i][0] + " " + songtitles.iloc[i][1])
+# make lists to store the relevant track features
+sp_track_id = []
+sp_acousticness = []
+sp_danceability = []
+sp_duration_ms = []
+sp_energy = []
+sp_instrumentalness = []
+sp_key = []
+sp_liveness = []
+sp_loudness = []
+sp_mode = []
+sp_speechiness = []
+sp_tempo = []
+sp_time_sig = []
+sp_valence = []
 
-result = sp.search(joined_titles[0], limit=1, type='track')
-features = sp.audio_features(result)
-pprint.pprint(result)
-pprint.pprint(features)
+print(len(songdata))
+
+for i in range(len(songdata)):
+    print(i, end="\r")
+    joined_titles.append(songdata.iloc[i][2] + " " + songdata.iloc[i][3])
+    result = sp.search(joined_titles[i], limit=1, type='track')
+
+    try:
+        uri = result['tracks']['items'][0]['uri']
+        features = sp.audio_features(uri)[0]
+        
+        sp_track_id.append(features['id'])
+        sp_acousticness.append(features['acousticness'])
+        sp_danceability.append(features['danceability'])
+        sp_duration_ms.append(features['duration_ms'])
+        sp_energy.append(features['energy'])
+        sp_instrumentalness.append(features['instrumentalness'])
+        sp_key.append(features['key'])
+        sp_liveness.append(features['liveness'])
+        sp_loudness.append(features['loudness'])
+        sp_mode.append(features['mode'])
+        sp_speechiness.append(features['speechiness'])
+        sp_tempo.append(features['tempo'])
+        sp_time_sig.append(features['time_signature'])
+        sp_valence.append(features['valence'])
+
+    except:
+        sp_track_id.append('NO TRACK FOUND ON SPOTIFY')
+        sp_acousticness.append(0)
+        sp_danceability.append(0)
+        sp_duration_ms.append(0)
+        sp_energy.append(0)
+        sp_instrumentalness.append(0)
+        sp_key.append(0)
+        sp_liveness.append(0)
+        sp_loudness.append(0)
+        sp_mode.append(0)
+        sp_speechiness.append(0)
+        sp_tempo.append(0)
+        sp_time_sig.append(0)
+        sp_valence.append(0)       
+
+songdata['sp_track_id'] = sp_track_id
+songdata['sp_acousticness'] = sp_acousticness
+songdata['sp_danceability'] = sp_danceability
+songdata['sp_duration_ms'] = sp_duration_ms
+songdata['sp_energy'] = sp_energy
+songdata['sp_instrumentalness'] = sp_instrumentalness
+songdata['sp_key'] = sp_key
+songdata['sp_liveness'] = sp_liveness
+songdata['sp_loudness'] = sp_loudness
+songdata['sp_mode'] = sp_mode
+songdata['sp_speechiness'] = sp_speechiness
+songdata['sp_tempo'] = sp_tempo
+songdata['sp_time_sig'] = sp_time_sig
+songdata['sp_valence'] = sp_valence
+
+songdata.to_csv(path_or_buf='data_with_features.csv')
