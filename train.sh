b8b60dc0ec73073a82a9e9c6f88c8d83486f1b8a
ShaurGaur <shauryavrat@live.com>
2021-02-25 20:13:53 -0800

    updated training files

--- train.sh
+++ train.sh
@@ -6,10 +6,11 @@
 #SBATCH -o train.out
 #SBATCH -e train.err
 #SBATCH --gres=gpu:1
-#SBATCH --nodelist=cn-m-1
 #SBATCH -t 4-00:00:00      #set max job time to 4 days, 0h (default is around 36-48h)
 
 #my commands
-source ./venv/bin/activate      #activate tensorflow environment
+#source ./venv/bin/activate      #activate tensorflow environment
+source activate tf_gpu
 python3 model.py
-deactivate
+#deactivate
+
