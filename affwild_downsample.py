a05925385ba7c1d7424ffebc8e6014c1888aa2b9
ShaurGaur <shauryavrat@live.com>
2022-01-19 16:07:28 -0800

    code for creating 30 downsamples

--- affwild_downsample.py
+++ affwild_downsample.py
@@ -10,69 +10,74 @@ import shutil
 import matplotlib as mpl
 
 root     = "./data/affwild"
-dest     = "./data/affholdout"
 ids      = [int(i) for i in os.listdir("{}/frames".format(root))]
 framecnt = pd.read_csv("{}/analysis/summary.csv".format(root), index_col=0, header=0, usecols=[0,1])
 allframe = pd.read_csv("{}/analysis/all.csv".format(root), header=0, index_col=0)
 
-idsUsed  = []
-sample   = {
-    "video": [],
-    "frame": [],
-    "arousal": [],
-    "valence": []
-}
+helper.makeDir("./data/affsamples")
 
-for idx in ids:
-    for frm in range(1, framecnt.loc[idx][0], 30):
-        fid = int(1e5 * idx + frm)
-        idsUsed.append(fid)
-        print(fid, end="\r")
+for num in range(0, 30):
+    dest     = "./data/affsamples/{:02d}".format(num)
+    helper.makeDir(dest)
 
-        sample["video"].append(idx)
-        sample["frame"].append(frm)
-        sample["arousal"].append(allframe.loc[fid]['arousal'])
-        sample["valence"].append(allframe.loc[fid]['valence'])
-
-        oldfile = "{}/frames/{}/{}.png".format(root, idx, frm).encode('unicode-escape')
-        newfile = "{}/frames/{}.png".format(dest, fid).encode('unicode-escape')
-        shutil.copyfile(oldfile, newfile)
-
-pd.DataFrame(sample, index=idsUsed).to_csv(path_or_buf="{}/data.csv".format(dest))
-
-stats = {}
-stats["count"] = len(idsUsed)
-for d in ["valence", "arousal"]:
-    stats[d] = {
-        "mean": np.nanmean(sample[d]),
-        "std": np.nanstd(sample[d]),
-        "var": np.nanvar(sample[d]),
-        "min": np.nanmin(sample[d]),
-        "max": np.nanmax(sample[d]),
-        "median": np.nanmedian(sample[d])        
+    idsUsed  = []
+    sample   = {
+        "video": [],
+        "frame": [],
+        "arousal": [],
+        "valence": []
     }
 
-json_obj = json.dumps(stats, indent=4)
-with open("{}/stats.json".format(dest), "w") as outfile:
-    outfile.write(json_obj)
+    for idx in ids:
+        for frm in range(num, framecnt.loc[idx][0], 30):
+            fid = int(1e5 * idx + frm)
+            idsUsed.append(fid)
+            print(fid, end="\r")
 
-helper.plot_AV_data(
-    sample["valence"], sample["arousal"],
-    title="Spread of AffWild Sample",
-    file="{}/cir.png".format(dest),
-    alpha=.05
-)
+            sample["video"].append(idx)
+            sample["frame"].append(frm)
+            sample["arousal"].append(allframe.loc[fid]['arousal'])
+            sample["valence"].append(allframe.loc[fid]['valence'])
 
-helper.plot_AV_box(
-    [sample["valence"], sample["arousal"]], ["valence","arousal"],
-    title="Distribution of AffWild Points",
-    file="{}/box.png".format(dest),
-)
+            oldfile = "{}/frames/{}/{}.png".format(root, idx, frm).encode('unicode-escape')
+            newfile = "{}/frames/{}.png".format(dest, fid).encode('unicode-escape')
+            shutil.copyfile(oldfile, newfile)
 
-hists = [(i, sample[i]) for i in ["valence", "arousal"]]
-for attr, arr in hists:
-    helper.graph(
-        xlabel=attr, ylabel="Count", data=arr, hist=True,
-        title="{} Distribution in Sample".format(attr), 
-        file="{}/{}.png".format(dest, attr)
+    pd.DataFrame(sample, index=idsUsed).to_csv(path_or_buf="{}/data.csv".format(dest))
+
+    stats = {}
+    stats["count"] = len(idsUsed)
+    for d in ["valence", "arousal"]:
+        stats[d] = {
+            "mean": np.nanmean(sample[d]),
+            "std": np.nanstd(sample[d]),
+            "var": np.nanvar(sample[d]),
+            "min": np.nanmin(sample[d]),
+            "max": np.nanmax(sample[d]),
+            "median": np.nanmedian(sample[d])        
+        }
+
+    json_obj = json.dumps(stats, indent=4)
+    with open("{}/stats.json".format(dest), "w") as outfile:
+        outfile.write(json_obj)
+
+    helper.plot_AV_data(
+        sample["valence"], sample["arousal"],
+        title="Spread of AffWild Sample",
+        file="{}/cir.png".format(dest),
+        alpha=.05
     )
+
+    helper.plot_AV_box(
+        [sample["valence"], sample["arousal"]], ["valence","arousal"],
+        title="Distribution of AffWild Points",
+        file="{}/box.png".format(dest),
+    )
+
+    hists = [(i, sample[i]) for i in ["valence", "arousal"]]
+    for attr, arr in hists:
+        helper.graph(
+            xlabel=attr, ylabel="Count", data=arr, hist=True,
+            title="{} Distribution in Sample".format(attr), 
+            file="{}/{}.png".format(dest, attr)
+        )
