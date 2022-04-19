fb3b54357e5b19e0bbbd7baf9f92468777ec53f2
ShaurGaur <shauryavrat@live.com>
2022-04-19 16:29:06 -0700

    fetched 10 songs from MSD

--- hdf5-deezer-msd.py
+++ hdf5-deezer-msd.py
@@ -1,26 +1,53 @@
 import hdf5_getters
 import pandas as pd
+import json
+import pprint
+import sys
 
 deezer_path = "./data/deezer/deezer-spotify.csv"
 msd_path = "../msd/data"
 attr_track = "MSD_track_id"
 attr_song = "MSD_sng_id"
 
-songdata = pd.read_csv(deezer_path, header=0, index_col=0).dropna()
-non_one = 0
+json_data_out = "deezer_msd_data.json"
+json_error_out = "deezer_msd_err.json"
 
-for i in range(len(songdata)):
-    print(i, end='\r')
+songdata = pd.read_csv(deezer_path, header=0, index_col=0).dropna()
+count = int(sys.argv[1]) if len(sys.argv) > 1 else len(songdata)
+
+attributes = [k[4:] for k in filter(lambda x : x[:4] == 'get_', hdf5_getters.__dict__.keys())]
+attributes.remove("num_songs")
+
+msdata = {}
+for a in attributes: msdata[a] = ["" for _ in range(count)]
+
+errorlog = []
+
+for i in range(count):
+    # print(i, end='\r')
     song = songdata.iloc[i]
     trackid = song[attr_track]
     
     h5path = "{}/{}/{}/{}/{}.h5".format(msd_path, trackid[2], trackid[3], trackid[4], trackid)
     h5 = hdf5_getters.open_h5_file_read(h5path)
 
-    count = hdf5_getters.get_num_songs(h5)
-    if count > 1:
-        non_one += 1
+    for a in attributes:
+        try:
+            res = hdf5_getters.__getattribute__("get_" + a)(h5, 0)
+            msdata[a][i] = res
+        except:
+            msdata[a][i] = None
+            errorlog.append((i, a, trackid))
 
     h5.close()
 
-print("Non one:", non_one)
+pprint.pprint(msdata)
+'''
+json_data = json.dumps(msdata, indent=4)
+with open(json_data_out, "w") as outfile:
+    outfile.write(json_data)
+
+json_error = json.dumps(errorlog, indent=4)
+with open(json_error_out, "w") as outfile:
+    outfile.write(json_error)
+'''
---
+++ hdf5-fetch.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH -J playlist      #job name
+#SBATCH -p cascades
+#SBATCH -A cascades
+#SBATCH -o hdf5-fetch.json
+#SBATCH -e hdf5-fetch.err
+#SBATCH -t 4-00:00:00      #set max job time to 4 days, 0h (default is around 36-48h)
+
+#my commands
+#source ./venv/bin/activate      #activate tensorflow environment
+source activate tf_gpu
+python3 hdf5-deezer-msd.py 10
+#deactivate
+
