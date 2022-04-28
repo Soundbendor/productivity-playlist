21916f81fab253865e7e4aba8a90937af1cfef9a
ShaurGaur <shauryavrat@live.com>
2022-04-28 12:25:42 -0700

    changes to msdeezer bash file

--- msdeezer.sh
+++ msdeezer.sh
@@ -3,12 +3,13 @@
 #SBATCH -J playlist      #job name
 #SBATCH -p cascades
 #SBATCH -A cascades
-#SBATCH -o msd-one.out
+#SBATCH -o msdeezerplus.out
+#SBATCH -e msdeezerplus.err
 #SBATCH -t 4-00:00:00      #set max job time to 4 days, 0h (default is around 36-48h)
 
 #my commands
 #source ./venv/bin/activate      #activate tensorflow environment
 source activate tf_gpu
-python3 msdeezer.py 1
+python3 msdeezer.py
 #deactivate
 
