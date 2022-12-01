0dd26c31b562057830a8b2ba394b45cd6059b578
ShaurGaur <shauryavrat@live.com>
2022-11-30 20:45:32 -0800

    get segment datasets

--- get_spot_segments.py
+++ get_spot_segments.py
@@ -53,18 +53,38 @@ def weighted_avg(arr, dur, sum, len):
     return (tot / (den * sum))
     # return (tot / (den * sum))
 
-def grab_segment_data(segments, mode = "cnt", num = 10):
+def grab_segment_data(segments, mode = "cnt", num = 10.0):
     outvals = {}
 
-    ## Weighted averages!
-    segs = {"head": segments[0:num], 
-            "tail": segments[(-1*num):]}
+    segs = {"head": [], 
+            "tail": []}
+    if mode == "cnt":
+        segs["head"] = segments[0:num] 
+        segs["tail"] = segments[(-1*num):]
+    
+    elif mode == "dur":
+        head_dur, tail_dur = 0.0, 0.0
+        head_idx, tail_idx = 0, len(segments) - 1
+        
+        while head_dur < num:
+            segs["head"].append(segments[head_idx])
+            head_dur += segments[head_idx]["duration"]
+            head_idx += 1
+
+        while tail_dur < num:
+            segs["tail"].append(segments[tail_idx])
+            tail_dur += segments[tail_idx]["duration"]
+            tail_idx -= 1
+    
+    # Get supplemental data for weighted averages.
     durs = {"head": [seg["duration"] for seg in segs["head"]], 
             "tail": [seg["duration"] for seg in segs["tail"]]}
     sums = {"head": sum(durs["head"]), 
             "tail": sum(durs["tail"])}
     lens = {"head": len(durs["head"]), 
             "tail": len(durs["tail"])}
+
+    # print("sums:", sums, "\nlens:", lens)
 
     for s in ["head", "tail"]:
         for feat in feats_ind: 
@@ -82,29 +102,32 @@ def grab_segment_data(segments, mode = "cnt", num = 10):
 
     return outvals
 
-def grab_dataset(outpath):
-    testlength  = 1
-
-    df = songdata.full_df[0:testlength].copy()
+def grab_dataset(outpath, length):
+    df = songdata.full_df[0:length].copy()
     outcols = {}
     fillcols("head", outcols)
     fillcols("tail", outcols)
 
-    for i in range(testlength): 
+    for i in range(length): 
         spid = df.iloc[i]["sp_track_id"]
-        analysis = sp.audio_analysis(spid)
-        segments = analysis["segments"]
-        print(i, end="\r")
+        
+        try:
+            analysis = sp.audio_analysis(spid)
+            segments = analysis["segments"]
+            print("{} / {}".format(i, length), end="\r")
 
-        vals = grab_segment_data(segments, mode = "cnt", num = 10)
-        for key in vals: outcols[key].append(vals[key])
+            vals = grab_segment_data(segments, mode = "dur", num = 5.0)
+            for key in vals: outcols[key].append(vals[key])
+        
+        except:
+            for key in outcols: outcols[key].append(None) 
 
     # EXPORT DATA TO CSV!!!
     for col in outcols: df[col] = outcols[col]
     df.to_csv(outpath)
 
 def test_segcounts(spid):
-    test_range = [x+1 for x in range(50)]
+    test_range = [x+1 for x in range(30)]
     segments = sp.audio_analysis(spid)["segments"]
     dirname = helper.makeTestDir("segcounts")
 
@@ -113,19 +136,21 @@ def test_segcounts(spid):
     fillcols("tail", testcols)  
       
     for r in test_range:
-        vals = grab_segment_data(segments, mode = "cnt", num = r)
+        vals = grab_segment_data(segments, mode = "dur", num = r)
         for key in vals: testcols[key].append(vals[key])
     
     for col in testcols:
-        plot.line("# of segments", col, [test_range, testcols[col]], 
+        plot.line("# of segments/seconds", col, [test_range, testcols[col]], 
                     count = 1, dim = 2,
                     title = "Spread of {}".format(col),
                     file="{}/{}.png".format(dirname, col))
         
-randidx = np.random.randint(0, len(songdata))
-randsong = songdata.full_df.iloc[randidx]
-print(randsong)
-test_segcounts(randsong["sp_track_id"])
+# randidx = np.random.randint(0, len(songdata))
+# randsong = songdata.full_df.iloc[randidx]
+# print(randsong)
+# test_segcounts(randsong["sp_track_id"])
+
+grab_dataset("out/deezer-segments.csv", len(songdata))
 
 
 
