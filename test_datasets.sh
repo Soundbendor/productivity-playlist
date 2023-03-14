aa812d8005bb2cf4f20f2aea07aab82e75ceed79
ShaurGaur <shauryavrat@live.com>
2023-03-14 11:01:13 -0700

    test datasets slurm file

---
+++ test_datasets.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH -J playlist      #job name
+#SBATCH -p cascades
+#SBATCH -A cascades
+#SBATCH -o ./out/test_datasets.out
+#SBATCH -e ./out/test_datasets.err
+#SBATCH -t 4-00:00:00      #set max job time to 4 days, 0h (default is around 36-48h)
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=gaurs@oregonstate.edu
+
+#my commands
+source ./venv/bin/activate      #activate tensorflow environment
+#source activate tf_gpu
+python3 test_datasets.py 25
+deactivate
+
