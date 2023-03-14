19ac5112559a63197e65f63213ea282e3c3fa34f
ShaurGaur <shauryavrat@live.com>
2023-03-14 16:02:50 -0700

    made more slurm files

--- run_distances.sh
+++ run_distances.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#SBATCH -J playlist-datasets      #job name
+#SBATCH -J playlist-distances      #job name
 #SBATCH -p cascades
 #SBATCH -A cascades
 #SBATCH -c 6
-#SBATCH -o ./log/test_datasets.out
-#SBATCH -e ./log/test_datasets.err
+#SBATCH -o ./log/test_distances.out
+#SBATCH -e ./log/test_distances.err
 #SBATCH -t 4-00:00:00      #set max job time to 4 days, 0h (default is around 36-48h)
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=gaurs@oregonstate.edu
@@ -13,6 +13,6 @@
 #my commands
 source ./venv/bin/activate      #activate tensorflow environment
 #source activate tf_gpu
-python3 test_datasets.py 25
+python3 test_distances.py 25
 deactivate
 
--- run_neighbors.sh
+++ run_neighbors.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#SBATCH -J playlist-datasets      #job name
+#SBATCH -J playlist-neighbors      #job name
 #SBATCH -p cascades
 #SBATCH -A cascades
 #SBATCH -c 6
-#SBATCH -o ./log/test_datasets.out
-#SBATCH -e ./log/test_datasets.err
+#SBATCH -o ./log/test_neighbors.out
+#SBATCH -e ./log/test_neighbors.err
 #SBATCH -t 4-00:00:00      #set max job time to 4 days, 0h (default is around 36-48h)
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=gaurs@oregonstate.edu
@@ -13,6 +13,6 @@
 #my commands
 source ./venv/bin/activate      #activate tensorflow environment
 #source activate tf_gpu
-python3 test_datasets.py 25
+python3 test_neighbors.py 25
 deactivate
 
